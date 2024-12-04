f7b82d388cfdd21d3930b7ca377b11d4af404642
Plensia Lukosi <plensiapl@gmail.com>
2024-12-04 09:19:30 +0300

    added js functionality to button

--- task 1_To_Do_List/script.js
+++ task 1_To_Do_List/script.js
@@ -5,3 +5,22 @@ inputBox.addEventListener("keyup", (e) => {
     if (e.key === "Enter") addTask();
   });
 //Add Task function
+function addTask() {
+    if (inputBox.value === '') {
+      alert("Please enter a task!");
+      return;
+    }
+    
+    const li = document.createElement("li");
+    li.innerHTML = `
+      ${inputBox.value}
+      <div class="actions">
+        <i class="bi bi-pencil" onclick="editTask(this)"></i>
+        <i class="bi bi-trash" onclick="deleteTask(this)"></i>
+      </div>
+    `;
+    
+    listContainer.appendChild(li);
+    inputBox.value = "";
+    saveData();
+  }
