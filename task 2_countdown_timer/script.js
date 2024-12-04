4af44ade30f0a6c7b6aa57dcbbe07b680226b912
Plensia Lukosi <plensiapl@gmail.com>
2024-12-04 14:06:15 +0300

    check whether user provides time/date

--- task 2_countdown_timer/script.js
+++ task 2_countdown_timer/script.js
@@ -8,4 +8,12 @@ function startCountdown(){
     clearInterval(countdownInterval);
    }
 }
+//get user input values from the form
+const datetimeInput = document.getElementById('datetime-input').value;
+const eventName = document.getElementById('event-input').value;
 
+//check if date/time was provided
+if(!datetimeInput){
+    alert('Please select a date and time');
+    return;
+}
